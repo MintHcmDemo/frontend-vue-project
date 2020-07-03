795c1b8349893d3c0ab171f18448519b4388373a
my_project <64954454+prakhardubey10@users.noreply.github.com>
2020-07-03 05:07:42 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import VueSession from 'vue-session'
 import Vuelidate from 'vuelidate'
-import i18n from '@/plugins/i18n'
-import { BootstrapVue } from 'bootstrap-vue'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronRight, faChevronDown, faSlidersH, faChild, faServer} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-library.add(faChevronRight, faChevronDown, faSlidersH, faChild, faServer);
+import Vuex from 'vuex'
+import Login from "./components/Login.vue"
+import HomePage from "./components/HomePage.vue"
+import Signup from "./components/Signup.vue"
+import ForgotPassword from "./components/ForgotPassword"
 
 
 import '@/assets/css/main.css'
@@ -21,13 +17,71 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(VueSession)
-Vue.use(Vuelidate)
-Vue.use(BootstrapVue)
+Vue.use(Vuelidate);
+Vue.use(Vuex);
+Vue.use(VueRouter);
+const store=new Vuex.Store(
 
+{
+  state:{
+    authenticated:false,
+  },
+  mutations:{
+    setAuthentication(state,status){
+      state.authenticated=status;
+    }
+  }
+}
+
+
+);
+
+const router = new VueRouter({
+
+routes:[
+  {
+    path:'/',
+    redirect:{
+      name:'login'
+    }
+
+  },
+ {
+   
+  path: '/register',
+    name: 'signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: ForgotPassword
+  },
+  {
+    path: '/home-page',
+    name: 'home-page',
+    component: HomePage,
+    beforeEnter: (to,from,next)=>{
+      if(store.state.authenticated==false){
+         next("/login");
+      }else{
+        next();
+      }
+    }
+  
+  }
+]
+});
 
 
 new Vue({
-  router,
+  
   render: h => h(App),
-  i18n
+store:store,
+router:router,
 }).$mount('#app')
