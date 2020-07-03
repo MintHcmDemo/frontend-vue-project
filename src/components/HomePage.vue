e51ed6ac8c000893725c3885f0acd97234dd7fc8
my_project <64954454+prakhardubey10@users.noreply.github.com>
2020-07-03 04:41:43 -0700

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,50 +1,17 @@
 <template>
     <div class="vue-tempalte">
-        
-            <h3>Welcome</h3>
 
-            <button  v-on:click.prevent="logout" class="btn btn-dark btn-lg btn-block">Logout</button>
-       
+            <h3>Welcome To the mintHCM </h3>
+
+             <button type="logout" class="btn btn-dark btn-lg btn-block">logout</button>
+
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {
-            
-            }
-        },
-        
-        beforeCreate: function() {
-
-            if (!this.$session.exists()) {
-            this.$router.push('/login')
-            }
-        },
-        methods: {
-            logout: function () {
-            this.$session.destroy()
-            this.$router.push('/login')
-            },
-            
-            callFunction: function () {
-                var self = this
-
-                setTimeout(function () {
-
-                    self.$session.destroy()
-                    self.$router.push('/login')
-
-                }, 10000);
-
-            }
-            
-        },
-        mounted () {
-              this.callFunction()
-
-        }   
-        
+            return {}
+        }
     }
 </script>
